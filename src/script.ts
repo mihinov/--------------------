3009bd19e9f935639eff81294cdb4afc698b9d24
mihinov <kamahinmihail@gmail.com>
2022-05-11 02:23:42 +0300

    first commit

---
+++ src/script.ts
@@ -0,0 +1,35 @@
+const inputStepNode: HTMLInputElement = document.querySelector('#input-step');
+const inputFromNode: HTMLInputElement = document.querySelector('#input-from');
+const inputToNode: HTMLInputElement = document.querySelector('#input-to');
+const formNode: HTMLFormElement = document.querySelector('#form');
+const formOutputNode: HTMLOutputElement = document.querySelector('#form-output');
+
+
+formNode.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const step = inputStepNode.valueAsNumber;
+	const from = inputFromNode.valueAsNumber;
+	const to = inputToNode.valueAsNumber;
+
+	const total = calcLadder(step, from, to);
+	formOutputNode.value = String(total);
+
+
+});
+
+function calcLadder(step: number, from: number, to: number): number {
+
+	if (step < 1) {
+		return 0;
+	}
+
+	let total = 0;
+	for (let count = from; count <= to; count += step) {
+		total += count;
+	}
+
+	return total;
+}
+
+
